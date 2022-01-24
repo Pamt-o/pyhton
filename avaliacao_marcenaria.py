4259bcea539eb6ba0945c47e474207a690638b7c
Pâmela Albuquerque <80077413+Pamt-o@users.noreply.github.com>
2022-01-24 19:35:42 -0300

    Add files via upload

---
+++ avaliacao_marcenaria.py
@@ -0,0 +1,11 @@
+avaliação = int(input("Digite a sua nota de avaliação sobre o serviço de marcenaria: "))
+media = 6
+
+if avaliação > media:
+    print("Excelente trabalho!")
+
+elif avaliação == media:
+    print("O serviço foi executado conforme o combinado.")
+
+else:
+    print("Pessimo atendimento, não recomendo.")
